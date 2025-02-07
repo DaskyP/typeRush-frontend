58dd8215c6c46c83fa0a31b4d935f7339874fce5
DaskyP <danteyahir123@gmail.com>
2025-02-07 13:57:10 -0600

    fiix:change modal height

--- src/components/ProfileModal.jsx
+++ src/components/ProfileModal.jsx
@@ -8,7 +8,7 @@ const ProfileModal = ({ isOpen, onClose }) => {
     <div className="fixed inset-0 flex items-center justify-center z-50">
       <div className="absolute inset-0 bg-opacity-50"></div>
 
-      <div className="relative bg-[#222222] w-[1000px] h-[600px] min-h-[600px] rounded-lg  border border-[#000000] p-6 flex flex-col">
+      <div className="relative bg-[#222222] w-[1000px] h-[650px] min-h-[600px] rounded-lg  border border-[#000000] p-6 flex flex-col">
         <ProfileTabs onClose={onClose} />
       </div>
     </div>
